d9ace80d6322091ef0c056792e5760d049c02cf4
tkrupesh14 <info.tkrupesh14@gmail.com>
2022-06-23 12:36:52 +0530

    45 Video Completed

---
+++ backend/models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const NotesSchema = new Schema({
+    title:{
+        type:string,
+        required: true
+    },
+    description: {
+        type:string,
+        required: true,
+
+    },
+    tag: {
+        type:string,
+        default: "General"
+    },
+    
+    date: {
+        type:Date,
+        required: Date.now
+
+    }
+  });
+
+  module.exports = mongoose.model('Notes', NotesSchema);
